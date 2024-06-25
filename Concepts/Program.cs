73878c9f944ae1b9ac23f0e0167aa3dc7f180099
Mohamme32 <168175923+Mohamme32@users.noreply.github.com>
2024-06-25 11:34:26 +0200

    mit tobias

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -1,2 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
 
+string name = "Mohammed";
+/* 
+ * DateTime ist einer von Vielen komplexen Datentypen in C#.
+ * DateTime ist ein Strukturtyp,
+ * Objekte können nur in der Konsole ausgegeben werden wenn sie eine Methode Tostring () haben.
+ */
+DateTime date = DateTime.Now;
+Console.WriteLine(date.Year);
+
+Console.WriteLine(name);
+
+// Funktionen 
+// x = 5y
+var x = Console.WriteLine;
