3c8d80333e89e0d35e5c8afad56ba90cc525115c
Mohamme32 <168175923+Mohamme32@users.noreply.github.com>
2024-06-26 14:55:03 +0200

    Este Aufgabe Matrix

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -13,4 +13,29 @@ Console.WriteLine(name);
 
 // Funktionen 
 // x = 5y
-var x = Console.WriteLine;
+/* Collections - Basics
+ * string [] namen = new string [5]; das ist die alteste Methode 
+ * Array ist eine Liste an Werten 
+ * Eine Kontinuirliche Speicherregion , für mehrere Daten eiens gewissen Types 
+ * // Collections-Basics
+// Array
+string[] namen = new string[5];
+string[] alternative_namen = ["Tobi", "Popstar", "Lehrer"]; // max 3 items
+string[] alternative_namen1 = { "Ab", "Bc", "Cd" };
+namen[0] = "Tobi";
+namen[1] = "Melanie";
+namen[2] = "Torben";
+namen[99999999999999998] = "Buxtehude!";
+Console.WriteLine(namen[0][0]);
+
+
+// Array ist eine Liste an Werten
+// Eine kontinuirliche Speicherregion, für mehrere Daten eines gewissen Types
+int[] nums = new int[5];
+*/
+//int x = 0;
+//x = x + 1;
+//x += 1;
+//x++;
+
+//Console.WriteLine("hi" + x)
